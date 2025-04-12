53ce3887f82600ad835629ed73615ea97356c9c2
Paweł <pawel.k.siewiera@gmail.com>
2025-04-12 21:38:53 +0200

    Dodanie komentarzy

--- Wezel.hpp
+++ Wezel.hpp
@@ -1,17 +1,10 @@
 #ifndef WEZEL_HPP
 #define WEZEL_HPP
-
-/**
- * @brief Prosty węzeł używany w listach.
- */
 struct Wezel {
-    int dane;    ///< Przechowuje dane.
-    Wezel* next; ///< Wskaźnik na następny węzeł.
+    int dane;
+    Wezel* next;
 
-    /**
-     * @brief Konstruktor węzła.
-     * @param wartosc Wartość przypisywana do węzła.
-     */
+
     Wezel(int wartosc) : dane(wartosc), next(nullptr) {}
 };
 
